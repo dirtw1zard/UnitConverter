058164b195959cf8bf54c8175e89002e3ec6a27e
Allison Brabham <abrabham@wvup.edu>
2026-09-16 20:03:29 -0400

    Saving changes

---
+++ src/UnitConverter/Pages/ConversionsModel.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnitConverter.Pages;
+
+public class ConversionsModel : PageModel
+{
+    public string Input { get; set; } = string.Empty;
+    public string Output { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+        Input = "3.1415";
+        ViewData.Add("ConversionType", "Miles to Kilometers");
+
+        Convert.ToDouble(Input);
+
+        Output = Convert.ToString(Input);
+    }
+}
